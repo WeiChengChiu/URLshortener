15a5ffc58d82480205e790baa2f4a77e55eba555
Eric Chiu <c3366301@gmail.com>
2016-07-12 21:28:11 +0800

    Add CRD and Views of URL-Shortener but can't see the shortURL

--- app/controllers/urlmaps_controller.rb
+++ app/controllers/urlmaps_controller.rb
@@ -1,2 +1,42 @@
 class UrlmapsController < ApplicationController
+
+  def index
+    @urlmap = Urlmap.new
+    @urlmaps = Urlmap.all
+  end
+
+  def create
+
+    @urlmap = Urlmap.new(urlmap_params)
+
+    begin
+      flag = false
+      rand_url = SecureRandom.hex(4)
+      Urlmap.all.each do |urlmap|
+        if ran_url == urlmap.short_url
+          flag = true
+          continue
+        end
+      end
+    end while flag == true
+
+    @urlmap.short_url = rand_url
+
+    redirect_to urlmaps_path
+  end
+
+  def destroy
+    @urlmap = Urlmap.find(params[:id])
+
+    @urlmap.destroy
+
+    redirect_to urlmaps_path
+  end
+
+  private
+
+  def urlmap_params
+    params.require(:urlmap).permit(:long_url, :short_url)
+  end
+
 end
--- db/migrate/20160712120943_create_urlmaps.rb
+++ db/migrate/20160712120943_create_urlmaps.rb
@@ -3,7 +3,6 @@ class CreateUrlmaps < ActiveRecord::Migration[5.0]
     create_table :urlmaps do |t|
       t.string :short_url
       t.string :long_url
-      t.integer :clicks
 
       t.timestamps
     end
